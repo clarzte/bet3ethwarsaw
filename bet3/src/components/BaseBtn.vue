7803c55b767c4e575dce5bb834c83b40f56d1e49
Arkadiusz Karwasz <akarwasz@netkodo.com>
2023-09-02 10:50:51 +0200

    init bet3

---
+++ bet3/src/components/BaseBtn.vue
@@ -0,0 +1,61 @@
+<template>
+  <button class="ripple-button" @click="addRippleEffect">
+    <span class="text"> {{ text }} </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseBtn",
+  props: {
+    text: { required: true, type: String },
+  },
+  methods: {
+    addRippleEffect(e) {
+      let button = e.currentTarget;
+
+      // Create ripple element
+      let ripple = document.createElement("span");
+      ripple.classList.add("ripple");
+
+      // Set ripple size and position
+      let rect = button.getBoundingClientRect();
+      let size = Math.max(rect.width, rect.height);
+      ripple.style.width = ripple.style.height = size + "px";
+      ripple.style.left = e.clientX - rect.left - size / 2 + "px";
+      ripple.style.top = e.clientY - rect.top - size / 2 + "px";
+
+      // Add to button and animate
+      button.appendChild(ripple);
+      ripple.addEventListener("animationend", () => {
+        ripple.remove();
+      });
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
+
+<style>
+.ripple-button {
+  position: relative;
+  overflow: hidden;
+  transform: translate3d(0, 0, 0);
+}
+
+.ripple {
+  position: absolute;
+  border-radius: 50%;
+  background: rgba(255, 255, 255, 0.7); /* you can use any color */
+  transform: scale(0);
+  animation: ripple-animation 0.6s linear;
+}
+
+@keyframes ripple-animation {
+  to {
+    transform: scale(4);
+    opacity: 0;
+  }
+}
+</style>
---
+++ bet3/src/views/HomeView.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="home">
+    <div class="logo-with-text">
+      <img alt="Vue logo" src="../assets/logo.png" class="logo" />
+      <h1 class="title">
+        Bet with your friends <br />
+        and earn rewards
+      </h1>
+    </div>
+    <BaseBtn class="login-btn" text="Log in with your Wallet" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import BaseBtn from "@/components/BaseBtn.vue";
+
+export default {
+  name: "HomeView",
+  components: { BaseBtn },
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  height: calc(100% - 32px);
+  padding: 16px;
+
+  .logo-with-text {
+    align-self: center;
+    .logo {
+      display: flex;
+      justify-self: flex-start;
+      width: 83px;
+      height: 83px;
+    }
+    .title {
+      font-size: 32px;
+      font-weight: 600;
+      line-height: normal;
+      letter-spacing: -0.64px;
+      color: white;
+      text-align: left;
+    }
+  }
+
+  .login-btn {
+    background: transparent;
+    border-radius: 50px;
+    border: 2px solid white;
+    color: white;
+    padding: 11px;
+
+    :deep(.text) {
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: normal;
+      letter-spacing: -0.32px;
+    }
+  }
+}
+</style>
