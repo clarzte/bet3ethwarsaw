2b33f11382bead960d00a877672f02b783e8a214
clarzte <senvill412@gmail.com>
2023-09-02 20:27:11 +0200

    Add deploy script

--- src/Bet3.sol
+++ src/Bet3.sol
@@ -187,7 +187,7 @@ contract Bet3 {
     function compare(
         string memory str1,
         string memory str2
-    ) public pure returns (bool) {
+    ) internal pure returns (bool) {
         return
             keccak256(abi.encodePacked(str1)) ==
             keccak256(abi.encodePacked(str2));
